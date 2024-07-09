660f3b17f61c17dddc39337818109f390877a56c
Dima K <@users.noreply.github.com>
2024-07-09 12:54:51 +0200

    Solved the debugging exercise

--- hello.py
+++ hello.py
@@ -4,9 +4,9 @@ import sys
 
 def full_name(first_name, last_name):
     """returns the full name"""
-    name = f"{first_name.capitalize()}{last_name.capitalize()}"
-    
-    return name
+
+    name = f"{first_name.capitalize()} {last_name.capitalize()}"
+    return name.strip()
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
@@ -14,7 +14,9 @@ if __name__ == "__main__":
         print(f'Hello{full_name("", "")}!')
     elif len(sys.argv) == 2:
         # => ['hello.py', 'John' ]
-        print(f'Hello {full_name(sys.argv[1], "")}!')
+        hello = f'Hello {full_name(sys.argv[1], "")}!'
+        print(hello)
+
     else:
-        # => ['hello.py', 'John', 'Lennon']
-        print(f"Hello {full_name(sys.argv[1], sys.argv[2])}!")
+        # => ['hello.py', 'John ', 'Lennon']
+        print(f'Hello {full_name(sys.argv[1], sys.argv[2])}!')
